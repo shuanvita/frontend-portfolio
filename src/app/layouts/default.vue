de03291ed97f9b09fe142aecb04e0270b3ca3466
shuanvi7 <shuanvi7@gmail.com>
2025-10-07 15:34:13 +0300

    Add Header and Footer

--- src/app/layouts/default.vue
+++ src/app/layouts/default.vue
@@ -1,11 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheHeader from '@/widgets/TheHeader/ui/TheHeader.vue';
+import TheFooter from '@/widgets/TheFooter/ui/TheFooter.vue';
+</script>
 
 <template>
-  <div class="bg-slate-200">
-    <header>Header</header>
-    <main>
-      <NuxtPage />
-    </main>
-    <footer>Footer</footer>
+  <div class="p-[15px] xl:p-[69px]">
+    <div class="border-1 border-1-line rounded-[8px]">
+      <TheHeader />
+      <main>
+        <NuxtPage />
+      </main>
+      <TheFooter />
+    </div>
   </div>
 </template>
