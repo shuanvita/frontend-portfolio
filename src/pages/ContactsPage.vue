bbef332ccfb0d672ddfb6408c5528eaee0afec33
Andrey Shurygin <andrey.sh@solargroup.team>
2025-11-27 12:58:48 +0300

    Разработка страницы "Контакты"

--- src/pages/ContactsPage.vue
+++ src/pages/ContactsPage.vue
@@ -1,6 +1,31 @@
 <script lang="ts" setup>
 import PageLayout from '@/app/layouts/PageLayout.vue'
 import { contactsData } from '@/shared/data'
+import { ref } from 'vue'
+
+const formData = ref({
+  name: '',
+  email: '',
+  message: '',
+})
+
+const classesForm = {
+  inner: 'border border-slate-700 rounded-lg',
+  outer: 'mb-[22px]',
+  input:
+    'p-3 bg-slate-950 w-full max-h-[120px] text-slate-400 rounded-lg text-[16px]/[1.5] resize-none',
+  label: 'flex mb-[7px] text-slate-400 text-[16px]/[1.5]',
+  validation: 'text-red-500 mt-1 text-xs',
+}
+
+const classesTextarea = {
+  inner: 'border border-slate-700 h-[120px] rounded-lg',
+  outer: 'mb-[23px]',
+  input:
+    'p-3 bg-slate-950 h-full w-full max-h-[120px] text-slate-400 rounded-lg text-[16px]/[1.5] resize-none',
+  label: 'flex mb-[7px] text-slate-400 text-[16px]/[1.5]',
+  validation: 'text-red-500 mt-1 text-xs',
+}
 </script>
 
 <template>
@@ -31,16 +56,14 @@ import { contactsData } from '@/shared/data'
     </template>
     <div class="grid min-h-[calc(100%-48px)] grid-cols-[632px_1fr]">
       <div class="grid place-content-center border-r-2 border-slate-700">
-        <div class="max-w-[372px]">
-          <FormKit type="form">
+        <div class="w-screen max-w-[372px] pt-[130px] pb-[239px]">
+          <FormKit type="form" v-model="formData" :actions="false" v-slot="{ invalid }">
             <FormKit
               name="name"
-              label-class="flex mb-[7px] text-slate-400 text-[16px]/[1.5]"
               label="_name:"
               validation="required"
-              inner-class="border border-slate-700 rounded-lg"
-              outer-class="mb-5"
-              input-class="p-3 bg-slate-950 text-slate-400 rounded-lg"
+              :classes="classesForm"
+              :validation-messages="{ required: 'Name is required' }"
               type="text"
               placeholder="Jonathan Davis"
             />
@@ -48,6 +71,8 @@ import { contactsData } from '@/shared/data'
               name="email"
               label="_email:"
               validation="required"
+              :validation-messages="{ required: 'Name is required' }"
+              :classes="classesForm"
               type="email"
               placeholder="jonathan_davis@gmail.com"
             />
@@ -55,8 +80,18 @@ import { contactsData } from '@/shared/data'
               name="message"
               label="_message"
               type="textarea"
+              :classes="classesTextarea"
               validation="required"
               placeholder="your message here..."
+            />
+            <FormKit
+              type="submit"
+              :disabled="Boolean(invalid)"
+              label="submit-message"
+              :classes="{
+                input:
+                  'bg-orange-300 text-slate-950 hover:bg-orange-200 text-[14px]/[1.4] flex max-h-10 cursor-pointer items-center justify-center rounded-lg px-3 py-2.5 duration-200',
+              }"
             />
           </FormKit>
         </div>
